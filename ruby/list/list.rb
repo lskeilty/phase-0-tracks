ba67244ac9c74508ee79e66eebb9781ea7cd267a
Leah Keilty <lskeilty@gmail.com>
2017-05-24 17:40:20 -0700

    Add list doc

---
+++ ruby/list/list.rb
@@ -0,0 +1,24 @@
+class TodoList
+
+  def initialize(list)
+    @list = ["do the dishes", "mow the lawn"]
+  end
+
+  def get_items
+    @list
+  end
+
+  def add_item(item)
+    @list << item
+  end
+
+  def delete_item(item)
+    @list.delete(item)
+    @list
+  end
+
+  def retrieve_item(index)
+    @list[index]
+  end
+
+end
