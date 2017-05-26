7d5f892e26c375ca3f90bb4150a774dd31ae2227
Leah Keilty <lskeilty@gmail.com>
2017-05-26 12:21:27 -0700

    finish 6.7 solo

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,8 +1,13 @@
 require_relative 'game'
 
-describe Game
+describe Game do
+  let(:game) {Game.new}
 
-  it "adds two integers" do
+ # it "adds two integers" do
+    #expect(game.add(3,4)).to eq 7
+  #end
+
+  it "provides feedback on word with _'s" do
+    expect(game.get_blankword("leah", "a")).to eq "  _  _ a _ "
   end
-
 end
